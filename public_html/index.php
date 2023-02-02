f5aeebb09a20b2a5334d4e7913567c83bc0700f2
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-02-02 17:26:06 -0300

    teste dump

--- public_html/index.php
+++ public_html/index.php
@@ -44,6 +44,8 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
+var_dump(__DIR__.'/../bootstrap/app.php');
+die;
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
