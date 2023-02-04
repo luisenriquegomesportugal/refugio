00afe82f031bf764f2945630799bf0f4fe3374bf
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-02-04 16:29:37 -0300

    window function

--- resources/views/refukids.blade.php
+++ resources/views/refukids.blade.php
@@ -202,7 +202,7 @@
                         </div>
                         <div class="d-flex align-items-center justify-content-start gap-2">
                             <button class="button button-primary" type="submit">Cadastrar</button>
-                            <a class="button button-primary mt-0" onclick="window.addResponsavel()">Inserir responsável</a>
+                            <div class="button button-primary mt-0" onclick="window.addResponsavel()">Inserir responsável</div>
                         </div>
                     </div>
                     <div class="col-12 col-md-5 mb-4 mb-md-0 offset-0 offset-md-1 position-relative">
