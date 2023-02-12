0247c069d7968567e957714a5775b2aebdb9c8d3
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-02-12 16:12:14 -0300

    adiciona a politica

--- resources/views/portal/politica.blade.php
+++ resources/views/portal/politica.blade.php
@@ -12,37 +12,38 @@
     </section>
     <!-- Form -->
     <section class="section section-lg bg-default">
-        <div class="container relative-container"><h2>Política Privacidade</h2>
-            <p>Endereço: <strong>Tv. Timbó, 1244, Pedreira, Belém</strong></p>
-            <p>A sua privacidade é importante para nós. É política do Refúgio Lifestyle respeitar a sua privacidade em
+        <div class="container relative-container">
+            <h2>Política Privacidade</h2>
+            <p class="text-justify">Endereço: <strong>Tv. Timbó, 1244, Pedreira, Belém</strong></p>
+            <p class="text-justify">A sua privacidade é importante para nós. É política do Refúgio Lifestyle respeitar a sua privacidade em
                 relação a qualquer informação sua que possamos coletar no site <a href="{{ env("APP_URL") }}"/>Refúgio
                 Lifestyle</a>, e outros sites que possuímos e operamos.</p>
-            <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço.
+            <p class="text-justify">Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço.
                 Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que
                 estamos coletando e como será usado.</p>
-            <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando
+            <p class="text-justify">Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando
                 armazenamos dados, protegemos dentro de meios comercialmente aceitáveis para evitar perdas e roubos, bem
                 como acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
-            <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando
+            <p class="text-justify">Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando
                 exigido
                 por lei.</p>
-            <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não
+            <p class="text-justify">O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não
                 temos
                 controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas
                 respectivas políticas de privacidade.</p>
-            <p>Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos
+            <p class="text-justify">Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos
                 fornecer alguns dos serviços desejados.</p>
-            <p>O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade
+            <p class="text-justify">O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade
                 e
                 informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações
                 pessoais, entre em contacto connosco.</p>
-            <h3>Mais informações</h3>
-            <p>Esperemos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem
+            <h3 class="mt-4">Mais informações</h3>
+            <p class="text-justify">Esperemos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem
                 certeza se
                 precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos
                 que
                 você usa em nosso site.</p>
-            <p>Esta política é efetiva a partir de <strong>Fevereiro</strong>/<strong>2023</strong>.</p>
+            <p class="text-justify">Esta política é efetiva a partir de <strong>Fevereiro</strong>/<strong>2023</strong>.</p>
         </div>
     </section>
 @endsection
