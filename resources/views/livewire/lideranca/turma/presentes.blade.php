88088b48cfd25cede907f4b4700c12d670b4d606
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-02-20 17:26:44 -0300

    página da presentes

--- resources/views/livewire/lideranca/turma/presentes.blade.php
+++ resources/views/livewire/lideranca/turma/presentes.blade.php
@@ -18,27 +18,18 @@
                 </tr>
                 </thead>
                 <tbody>
-                @foreach($membros as $membro)
-                    <tr>
-                        <td>
-                            {{ $membro->nome }}
-                        </td>
-                        <td>
-                            {{ \Carbon\Carbon::parse($membro->nascimento)->format('d/m/Y') }}
-                        </td>
-                        @if(\Carbon\Carbon::parse($turmaChamada->dia)->isCurrentDay())
+                @if(\Carbon\Carbon::parse($turmaChamada->dia)->isCurrentDay())
+                    @foreach($membros as $membro)
+                        <tr>
+                            <td>
+                                {{ $membro->nome }}
+                            </td>
+                            <td>
+                                {{ \Carbon\Carbon::parse($membro->nascimento)->format('d/m/Y') }}
+                            </td>
                             <td>
                                 <div class="d-flex action-button">
                                     @if($turmaChamada->presentes->contains($membro))
-                                        <a wire:click.prevent="removerPresenca({{$membro->id}})"
-                                           class="btn btn-danger btn-xs light px-2">
-                                            <i class="fa fa-trash me-2" wire:loading.remove
-                                               wire:target="removerPresenca({{$membro->id}})"></i>
-                                            <i class="fa fa-spinner fa-spin me-2" wire:loading
-                                               wire:target="removerPresenca({{$membro->id}})"></i>
-                                            Remover
-                                        </a>
-                                    @else
                                         <a wire:click.prevent="cadastrarPresenca({{$membro->id}})"
                                            class="btn btn-success btn-xs light px-2">
                                             <i class="fa fa-check me-2" wire:loading.remove
@@ -47,12 +38,32 @@
                                                wire:target="cadastrarPresenca({{$membro->id}})"></i>
                                             Presente
                                         </a>
+                                    @else
+                                        <a wire:click.prevent="removerPresenca({{$membro->id}})"
+                                           class="btn btn-danger btn-xs light px-2">
+                                            <i class="fa fa-times me-2" wire:loading.remove
+                                               wire:target="removerPresenca({{$membro->id}})"></i>
+                                            <i class="fa fa-spinner fa-spin me-2" wire:loading
+                                               wire:target="removerPresenca({{$membro->id}})"></i>
+                                            Faltou
+                                        </a>
                                     @endif
                                 </div>
                             </td>
-                        @endif
-                    </tr>
-                @endforeach
+                        </tr>
+                    @endforeach
+                @else
+                    @foreach($turmaChamada->presentes as $membro)
+                        <tr>
+                            <td>
+                                {{ $membro->nome }}
+                            </td>
+                            <td>
+                                {{ \Carbon\Carbon::parse($membro->nascimento)->format('d/m/Y') }}
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
                 </tbody>
             </table>
         </div>
