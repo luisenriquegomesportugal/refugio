ece060a44c3be97480ae0db9cac556a23d1396be
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-03-05 18:46:27 -0300

    acesso

--- resources/views/livewire/lideranca/modals/configuracao/liberar-acesso-permissoes.blade.php
+++ resources/views/livewire/lideranca/modals/configuracao/liberar-acesso-permissoes.blade.php
@@ -57,42 +57,42 @@
                 @endforeach
                 </tbody>
             </table>
-            <div class="modal fade z-3" id="liberar-acesso-permissoes" wire:ignore.self>
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Alterar permissões</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal">
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="widget-media">
-                                <ul class="timeline">
-                                    @foreach(\App\Models\Permissao::orderBy('nome')->get() as $permissao)
-                                        <li>
-                                            <div class="timeline-panel">
-                                                <div
-                                                    class="custom-control custom-checkbox checkbox-warning check-lg me-3">
-                                                    <input type="checkbox"
-                                                           class="custom-control-input"
-                                                           id="usuario-{{$usuario->id}}-permissao-{{$permissao->id}}"
-                                                           wire:click.prevent="salvarPermissoes('{{ $permissao->id }}', '{{ $usuario->permissoes->contains($permissao) }}')"
-                                                           wire:loading.remove
-                                                           wire:target="salvarPermissoes('{{ $permissao->id }}', '{{ $usuario->permissoes->contains($permissao) }}')"
-                                                        @checked($usuario->permissoes->contains($permissao))>
-                                                    <i class="fa fa-spinner fa-spin" wire:loading
-                                                       wire:target="salvarPermissoes('{{ $permissao->id }}', '{{ $usuario->permissoes->contains($permissao) }}')"></i>
-                                                    <label class="custom-control-label"
-                                                           for="usuario-{{$usuario->id}}-permissao-{{$permissao->id}}"></label>
-                                                </div>
-                                                <div class="media-body">
-                                                    <h5 class="mb-0">{{$permissao->nome}}</h5>
-                                                </div>
+        </div>
+        <div class="modal fade" id="liberar-acesso-permissoes" wire:ignore.self>
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Alterar permissões</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal">
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="widget-media">
+                            <ul class="timeline">
+                                @foreach(\App\Models\Permissao::orderBy('nome')->get() as $permissao)
+                                    <li>
+                                        <div class="timeline-panel">
+                                            <div
+                                                class="custom-control custom-checkbox checkbox-warning check-lg me-3">
+                                                <input type="checkbox"
+                                                       class="custom-control-input"
+                                                       id="usuario-{{$usuario->id}}-permissao-{{$permissao->id}}"
+                                                       wire:click.prevent="salvarPermissoes('{{ $permissao->id }}', '{{ $usuario->permissoes->contains($permissao) }}')"
+                                                       wire:loading.remove
+                                                       wire:target="salvarPermissoes('{{ $permissao->id }}', '{{ $usuario->permissoes->contains($permissao) }}')"
+                                                    @checked($usuario->permissoes->contains($permissao))>
+                                                <i class="fa fa-spinner fa-spin" wire:loading
+                                                   wire:target="salvarPermissoes('{{ $permissao->id }}', '{{ $usuario->permissoes->contains($permissao) }}')"></i>
+                                                <label class="custom-control-label"
+                                                       for="usuario-{{$usuario->id}}-permissao-{{$permissao->id}}"></label>
                                             </div>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                            </div>
+                                            <div class="media-body">
+                                                <h5 class="mb-0">{{$permissao->nome}}</h5>
+                                            </div>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
                         </div>
                     </div>
                 </div>
