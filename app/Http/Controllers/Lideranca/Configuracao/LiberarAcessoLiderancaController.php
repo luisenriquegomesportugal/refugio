2060050041d0b7ce69ce47ff454d3899cdb2d28c
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-03-05 18:39:38 -0300

    acesso

--- app/Http/Controllers/Lideranca/Configuracao/LiberarAcessoLiderancaController.php
+++ app/Http/Controllers/Lideranca/Configuracao/LiberarAcessoLiderancaController.php
@@ -11,7 +11,7 @@ class LiberarAcessoLiderancaController extends Controller
     public function pagina()
     {
         $usuarios = User::with('permissoes')
-            ->paginate();
+            ->get();
 
         return view('lideranca.configuracao.liberar-acesso', compact('usuarios'));
     }
