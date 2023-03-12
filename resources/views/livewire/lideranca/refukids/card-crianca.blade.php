dad7aec6453189306f222cbfce525cf4cd130a36
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-03-12 18:21:57 -0300

    layout card

--- resources/views/livewire/lideranca/refukids/card-crianca.blade.php
+++ resources/views/livewire/lideranca/refukids/card-crianca.blade.php
@@ -1,4 +1,4 @@
-<div class="card m-0">
+<div class="card m-2 m-sm-0">
     <div class="project-info column-gap-3 row-gap-3 row-gap-sm-0">
         <div class="d-flex col-lg-3 col-sm-10 col-12 order-0 order-lg-0">
             <div class="d-flex flex-grow-1 flex-lg-grow-0 flex-row-reverse flex-sm-row align-items-start gap-3">
