602ea7e6fee64f81698532acbbef3e79860aca28
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-03-12 18:57:14 -0300

    listagem refukids

--- resources/views/lideranca/refukids/index.blade.php
+++ resources/views/lideranca/refukids/index.blade.php
@@ -1,39 +1,31 @@
-@extends('layouts.portal')
+@extends('layouts.lideranca')
 @section('titulo', 'Refukids')
 
 @section('conteudo')
-    <!-- Breadcrumbs -->
-    <section class="section section-bredcrumbs shadow"
-        style="background: linear-gradient(#000000DD 20%, #00000088 100%), url(assets/portal/images/breadcrumbs-1920x440.jpg) no-repeat center / cover;">
-        <div class="container context-dark breadcrumb-wrapper text-left">
-            <h1>Refukids</h1>
-            <div class="line"></div>
-        </div>
-    </section>
-    <!-- Form -->
-    <section class="section section-lg bg-default">
-        <div class="container relative-container">
-            <div class="row row-30 row-md-60 justify-content-between">
-                <div class="col-md-12">
-                    <h2>Listagem da Refukids</h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <table class="table table-borderless">
-                        <tbody>
-                            @foreach ($refukids as $crianca)
-                            <tr>
-                                <td>
-                                    @livewire('lideranca.refukids.card-crianca', ['membro' => $crianca], key($crianca->id))
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {!! $refukids->links('pagination::default') !!}
+    <div class="container-fluid">
+        <div class="row page-titles mx-0">
+            <div class="col-sm-6 p-md-0">
+                <div class="welcome-text">
+                    <h4>Refukids</h4>
+                    <span>Crianças cadastradas</span>
                 </div>
             </div>
         </div>
-    </section>
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-borderless">
+                    <tbody>
+                    @foreach ($refukids as $crianca)
+                        <tr>
+                            <td>
+                                @livewire('lideranca.refukids.card-crianca', ['membro' => $crianca], key($crianca->id))
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                {!! $refukids->links('pagination::default') !!}
+            </div>
+        </div>
+    </div>
 @endsection
